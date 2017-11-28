e41989617f124ba6490a1463cb05bb42c94e366e
Thomas Friedel <ralino96@googlemail.com>
2017-11-28 23:32:24 +0100

    Rewrite WorkspaceManager

--- src/WorkspaceManager.cpp
+++ src/WorkspaceManager.cpp
@@ -7,7 +7,6 @@ WorkspaceManager::WorkspaceManager(NameFinder name_finder,
                                    std::shared_ptr<i3ipc::connection> i3_conn)
   : m_name_finder(name_finder)
   , m_i3_conn(i3_conn)
-  , m_workspaces()
 {
   updateAllWorkspaces();
 }
@@ -15,34 +14,31 @@ WorkspaceManager::WorkspaceManager(NameFinder name_finder,
 void WorkspaceManager::updateAllWorkspaces()
 {
   m_workspaces.clear();
-  for (unsigned index = 0; index < 10; index++)
-  {
-    m_workspaces.emplace_back(index);
-  }
+  m_window_ids.clear();
+
   auto root = m_i3_conn->get_tree();
-  unsigned ws_number = 10;
+  unsigned last_ws_number = 0;
   bool set_name = true;
   std::function<void(std::shared_ptr<i3ipc::container_t>)> depth_first = [&] (std::shared_ptr<i3ipc::container_t> root) -> void
   {
     std::cout << root->name << ", " << root->ws_num << ", " << (root->xwindow_id? root->window_properties.wm_class : "")  << std::endl;
     if (root->type == "workspace" && root->name != "__i3_scratch")
     {
-      if (root->ws_num < 1 || root->ws_num > 10)
-      {
-        std::cerr << "Unexpected ws number" << std::endl;
-        return;
-      }
+      auto new_workspace = std::make_shared<Workspace>();
+      new_workspace->ws_number = root->ws_num;
+      new_workspace->current_name = root->name;
+      m_workspaces.emplace(new_workspace->ws_number, new_workspace);
+
       if (!set_name) //Check if last workspace's name was set
       {
-        setWorkspaceName("", ws_number);
+        updateWorkspaceName("", last_ws_number);
       }
       set_name = false;
-      ws_number = root->ws_num - 1;
-      m_workspaces[ws_number].current_name = root->name;
+      last_ws_number = new_workspace->ws_number;
     }
-    if (root->xwindow_id && ws_number < 10)
+    if (root->xwindow_id && last_ws_number != 0)
     {
-      addWindow(root->xwindow_id, root->window_properties.wm_class, root->window_properties.title, ws_number);
+      addWindow(root->id, root->window_properties.wm_class, root->window_properties.title, last_ws_number);
       set_name = true;
     }
     for (auto child : root->nodes)
@@ -57,99 +53,269 @@ void WorkspaceManager::updateAllWorkspaces()
   depth_first(root);
 }
 
-void WorkspaceManager::setWorkspaceName(std::string new_name, unsigned ws_number)
+void WorkspaceManager::updateWorkspaceName(std::string new_name, unsigned ws_number)
 {
   if (new_name.empty())
   {
     new_name = "Empty";
   }
   std::stringstream ss;
-  ss << ws_number + 1 << ": " << new_name;
+  ss << ws_number << ": " << new_name;
 
   //if (std::string(ss.str()) != m_workspaces[ws_number].current_name)
   //{
-  m_workspaces[ws_number].current_name = ss.str();
-  updateWorkspace(ws_number);
+  //TODO set actual name with i3_conn
+  std::cout << "Set a new name: \"" << ss.str() << "\"" << std::endl;
   //}
 }
 
-void WorkspaceManager::updateWorkspace(unsigned ws_number)
+void WorkspaceManager::initWorkspace(unsigned ws_num, std::string name)
 {
-  //TODO set actual name with i3_conn
-  std::cout << "Set a new name: \"" << m_workspaces[ws_number].current_name << "\"" << std::endl;
+  if (m_workspaces.find(ws_num) != m_workspaces.cend())
+  {
+    std::cerr << "Trying to initialize a workspace which already exists" << std::endl;
+    return;
+  }
+  auto workspace = std::make_shared<Workspace>();
+  workspace->ws_number = ws_num;
+  workspace->current_name = name;
+  m_workspaces.emplace(ws_num, workspace);
+
+  this->updateWorkspaceName("", ws_num);
+}
+
+void WorkspaceManager::removeWorkspace(unsigned ws_num)
+{
+  std::shared_ptr<Workspace> workspace;
+  try
+  {
+    workspace = m_workspaces.at(ws_num);
+  }
+  catch (std::out_of_range e)
+  {
+    std::cerr << "Removing non existing workspace" << std::endl;
+    return;
+  }
+
+  if (!workspace->window_indices.empty())
+  {
+    std::cerr << "Removing workspace which still contains windows" << std::endl;
+    //Continuing anyway
+
+    for (auto pair : workspace->window_indices)
+    {
+      this->removeWindow(pair.second->id);
+    }
+  }
+
+  m_workspaces.erase(ws_num);
+}
+
+void WorkspaceManager::changeWorkspaceName(unsigned ws_num, std::string new_name)
+{
+  std::shared_ptr<Workspace> workspace;
+  try
+  {
+    workspace = m_workspaces.at(ws_num);
+  }
+  catch (std::out_of_range e)
+  {
+    std::cerr << "Changing name of non existing workspace" << std::endl;
+    return;
+  }
+
+  workspace->current_name = new_name;
 }
 
 void WorkspaceManager::addWindow(
     uint64_t id, std::string wm_class, std::string title, unsigned ws_number)
 {
+  if (ws_number == 0)
+  {
+    ws_number = findWindow(id);
+  }
+
+  if (m_workspaces.find(ws_number) == m_workspaces.cend())
+  {
+    std::cerr << "Adding window to workspace which does not exist" << std::endl;
+    return;
+  }
+
   std::shared_ptr<Window> new_window = std::make_shared<Window>();
   new_window->id = id;
+  new_window->ws_number = ws_number;
   new_window->wm_class = wm_class;
   new_window->title = title;
   new_window->name_index = m_name_finder.getNameIndex(wm_class, title);
 
-  m_workspaces[ws_number].window_ids.insert(new_window);
+  m_window_ids.emplace(new_window->id, new_window);
 
-  auto it = m_workspaces[ws_number].window_indices.insert(new_window);
-  if (it == m_workspaces[ws_number].window_indices.cbegin())
+  auto it = m_workspaces.at(ws_number)->window_indices.emplace(new_window->name_index, new_window);
+  if (it == m_workspaces.at(ws_number)->window_indices.cbegin())
   {
     std::string new_ws_name = m_name_finder.getName(new_window->name_index);
     if (new_ws_name == "CLASS")
     {
       new_ws_name = new_window->wm_class;
     }
-    setWorkspaceName(new_ws_name, ws_number);
+    updateWorkspaceName(new_ws_name, ws_number);
   }
 }
 
-void WorkspaceManager::removeWindow(uint64_t id, unsigned ws_number)
+void WorkspaceManager::moveWindow(uint64_t id)
 {
-  std::shared_ptr<Window> comp_win = std::make_shared<Window>();
-  comp_win->id = id;
-
-  auto id_it = m_workspaces[ws_number].window_ids.find(comp_win);
-  if (id_it == m_workspaces[ws_number].window_ids.cend())
+  std::shared_ptr<Window> win;
+  try
   {
-    std::cerr << "Could not found window " << id << " on workspace " << ws_number << std::endl;
+    win = m_window_ids.at(id);
+  }
+  catch (std::out_of_range e)
+  {
+    std::cerr << "Moving non existing window" << std::endl;
     return;
   }
-  else
-  {
-    auto it = m_workspaces[ws_number].window_indices.find(*id_it);
-    if (it == m_workspaces[ws_number].window_ids.cbegin())
-    {
-      auto next = it;
-      next++;
-      if (next == m_workspaces[ws_number].window_ids.cend())
-      {
-        //Removing last window
-        setWorkspaceName("", ws_number);
-      }
-      else
-      {
-        std::string new_ws_name = m_name_finder.getName((*next)->name_index);
-        if (new_ws_name == "CLASS")
-        {
-          new_ws_name = (*next)->wm_class;
-        }
-        setWorkspaceName(new_ws_name, ws_number);
-      }
 
-      m_workspaces[ws_number].window_ids.erase(id_it);
-      m_workspaces[ws_number].window_indices.erase(it);
+  std::shared_ptr<Workspace> new_ws;
+  try
+  {
+    new_ws = m_workspaces.at(findWindow(win->id));
+  }
+  catch (std::out_of_range e)
+  {
+    std::cerr << "Moving a window to a non existing workspace" << std::endl;
+    return;
+  }
+
+  if (new_ws->ws_number != win->ws_number)
+  {
+    this->removeWindow(win->id);
+    this->addWindow(win->id, win->wm_class, win->title, new_ws->ws_number);
+  }
+}
+
+void WorkspaceManager::changeWindowTitle(uint64_t id, std::string new_title)
+{
+  auto pair = m_window_ids.find(id);
+  if (pair == m_window_ids.cend())
+  {
+    std::cerr << "Tried to change window title of non existing window" << std::endl;
+    return;
+  }
+  auto win = pair->second;
+  win->title = new_title;
+  auto new_index = m_name_finder.getNameIndex(win->wm_class, win->title);
+  if (new_index != (win)->name_index)
+  {
+    (win)->name_index = new_index;
+    std::shared_ptr<Workspace> workspace;
+    try
+    {
+      workspace = m_workspaces.at(win->ws_number);
+    }
+    catch (std::out_of_range e)
+    {
+      std::cerr << "Changing window title on non existing workspace" << std::endl;
+      return;
+    }
+    auto it = workspace->window_indices.emplace(win->name_index, win);
+    if (it == workspace->window_indices.cbegin())
+    {
+      std::string new_ws_name = m_name_finder.getName((win)->name_index);
+      if (new_ws_name == "CLASS")
+      {
+        new_ws_name = (win)->wm_class;
+      }
+      updateWorkspaceName(new_ws_name, (win)->ws_number);
     }
   }
 }
 
-WorkspaceManager::Workspace::Workspace(unsigned index)
-  : current_name(std::to_string(index + 1))
-  , window_ids([](std::shared_ptr<Window> lhs, std::shared_ptr<Window> rhs) -> bool
-                {
-                  return lhs->id < rhs->id;
-                })
-  , window_indices([](std::shared_ptr<Window> lhs, std::shared_ptr<Window> rhs) -> bool
-                    {
-                      return lhs->name_index < rhs->name_index;
-                    })
+unsigned WorkspaceManager::removeWindow(uint64_t id)
 {
+  std::shared_ptr<Window> win;
+  try
+  {
+    win = m_window_ids.at(id);
+  }
+  catch (std::out_of_range e)
+  {
+    std::cerr << "Could not find window " << id << std::endl;
+    return 0;
+  }
+  std::shared_ptr<Workspace> workspace;
+  try
+  {
+    workspace = m_workspaces.at(win->ws_number);
+  }
+  catch (std::out_of_range e)
+  {
+    std::cerr << "Removing window from non existing workspace" << std::endl;
+    return 0;
+  }
+  auto it = workspace->window_indices.find(win->name_index);
+  if (it == workspace->window_indices.cbegin())
+  {
+    auto next = it;
+    next++;
+    if (next == workspace->window_indices.cend())
+    {
+      //Removing last window
+      updateWorkspaceName("", workspace->ws_number);
+    }
+    else
+    {
+      std::string new_ws_name = m_name_finder.getName(next->second->name_index);
+      if (new_ws_name == "CLASS")
+      {
+        new_ws_name = next->second->wm_class;
+      }
+      updateWorkspaceName(new_ws_name, workspace->ws_number);
+    }
+  }
+
+  m_window_ids.erase(id);
+  workspace->window_indices.erase(it);
+
+  return workspace->ws_number;
+}
+
+unsigned WorkspaceManager::findWindow(uint64_t win_id)
+{
+  unsigned ws_number = 0;
+  std::function<unsigned(std::shared_ptr<i3ipc::container_t>)> depth_first = [&] (std::shared_ptr<i3ipc::container_t> root) -> unsigned
+  {
+    if (root->type == "workspace" && root->name != "__i3_scratch")
+    {
+      if (root->ws_num < 1)
+      {
+        std::cerr << "Unexpected ws number" << std::endl;
+        return 0;
+      }
+      ws_number = root->ws_num;
+    }
+    if (root->id == win_id)
+    {
+      //Found window
+      return ws_number;
+    }
+    for (auto child : root->nodes)
+    {
+      auto ret = depth_first(child);
+      if (ret != 0)
+      {
+        return ret;
+      }
+    }
+    for (auto floating_child : root->floating_nodes)
+    {
+      auto ret = depth_first(floating_child);
+      if (ret != 0)
+      {
+        return ret;
+      }
+    }
+    return 0;
+  };
+
+  return depth_first(m_i3_conn->get_tree());
 }
--- src/WorkspaceManager.h
+++ src/WorkspaceManager.h
@@ -16,33 +16,52 @@ public:
 
   /**
    * Reset the cached window tree and rebuild it from i3ipc.get_tree
-   * Re-sets all workspace names
+   * Resets all workspace names
    **/
   void updateAllWorkspaces();
   /**
    * Sets a new name for the workspace
    **/
-  void setWorkspaceName(std::string new_name, unsigned ws_number);
+  void updateWorkspaceName(std::string new_name, unsigned ws_number);
   /**
-   * Tell i3 about the workspace name for workspace {@param ws_number}
+   * Remove workspace
    **/
-  void updateWorkspace(unsigned ws_number);
-
+  void removeWorkspace(unsigned ws_num);
   /**
-   * Add a new window to the workspace {@param ws_number}.
+   * register new workspace
+   **/
+  void initWorkspace(unsigned ws_num, std::string name);
+  /**
+   * Changes workspace name (when the workspace is changed from a different process)
+   **/
+  void changeWorkspaceName(unsigned ws_num, std::string new_name);
+  /**
+   * Add a new window to the workspace ws_number. If ws_number is 0 (default),
+   * this will search for the window in the tree and add it to the appropriate workspace
    * Updates the workspace name if necessary
    **/
-  void addWindow(uint64_t id, std::string wm_class, std::string title, unsigned ws_number);
+  void addWindow(uint64_t id, std::string wm_class, std::string title, unsigned ws_number = 0);
   /**
-   * Removes the window {@param id} from the workspace {@param ws_number}.
+   * Change title to {@param new_title} of window {@param id}.
    * Updates the workspace name if necessary
    **/
-  void removeWindow(uint64_t id, unsigned ws_number);
+  void changeWindowTitle(uint64_t id, std::string new_title);
+  /**
+   * Removes the window {@param id}
+   * Updates the workspace name if necessary
+   * @return workspace number the window was removed from
+   **/
+  unsigned removeWindow(uint64_t id);
+  /**
+   * Move existing window
+   **/
+  void moveWindow(uint64_t id);
 
 private:
   struct Window
   {
     uint64_t id;
+    unsigned ws_number;
     std::string wm_class;
     std::string title;
 
@@ -50,15 +69,18 @@ private:
   };
   struct Workspace
   {
-    Workspace(unsigned index);
-
+    unsigned ws_number;
     std::string current_name;
-    std::set<std::shared_ptr<Window>, std::function<bool(std::shared_ptr<Window>, std::shared_ptr<Window>)> > window_ids;
-    std::multiset<std::shared_ptr<Window>, std::function<bool(std::shared_ptr<Window>, std::shared_ptr<Window>)> > window_indices;
+    std::multimap<NameFinder::NameIndex, std::shared_ptr<Window>> window_indices;
   };
 
   NameFinder m_name_finder;
   std::shared_ptr<i3ipc::connection> m_i3_conn;
 
-  std::vector<Workspace> m_workspaces;
+  std::map<uint64_t, std::shared_ptr<Window>> m_window_ids;
+
+  std::map<unsigned, std::shared_ptr<Workspace>> m_workspaces;
+
+  //helper funcs
+  unsigned findWindow(uint64_t win_id);
 };
